f5e7ae89d79a341194a58964ffe1f9ee3c5dd516
Duncan McGreggor <oubiwann@gmail.com>
2016-01-18 21:34:15 -0600

    Updated the location of doc builds and deployments.

--- resources/make/docs.mk
+++ resources/make/docs.mk
@@ -4,7 +4,9 @@ DOCS_DIR = $(ROOT_DIR)/docs
 USER_GUIDE_DIR = $(DOCS_DIR)/user-guide
 
 # The following dir is for build only and can be destroyed
-GUIDE_BUILD_DIR = $(USER_GUIDE_DIR)/build
+DOCS_BUILD_DIR = $(DOCS_DIR)/build
+GUIDE_BUILD_DIR = $(DOCS_BUILD_DIR)/user-guide
+API_BUILD_DIR = $(DOCS_BUILD_DIR)/api
 
 # The following dir is tracked by the project git (not the ephemeral git repo
 # created for pushing to gh-pages) and CANNOT BE DESTROYED -- this is where the
@@ -19,9 +21,13 @@ GUIDE_BUILD_DIR = $(USER_GUIDE_DIR)/build
 # Note that /current is not tracked in the repo (there is a .gitignore for
 # docs/master/current. Everything else in the layout above, however, is tracked
 # in git.
+CURRENT_VERSION = current
 DOCS_PROD_DIR = $(DOCS_DIR)/master
+CURRENT_PROD_DIR = $(DOCS_PROD_DIR)/$(CURRENT_VERSION)
+GUIDE_PROD_DIR = $(CURRENT_PROD_DIR)/user-guide
+API_PROD_DIR = $(CURRENT_PROD_DIR)/api-guide
 
-docs: clean-docs api-docs user-guide
+docs: clean-docs $(DOCS_BUILD_DIR) api-docs $(GUIDE_BUILD_DIR) user-guide
 
 publish-docs: $(DOCS_PROD_DIR) docs setup-temp-repo
 	@echo "\nPublishing docs ...\n"
@@ -35,13 +41,25 @@ api-docs:
 	@echo "rebar3 lfe lodox"
 
 SLATE_GIT_HACK = $(DOCS_DIR)/.git
-CURRENT_VERSION = current
+
+$(DOCS_BUILD_DIR):
+	@mkdir -p $(DOCS_BUILD_DIR)
+
+$(GUIDE_BUILD_DIR):
+	@mkdir -p $(GUIDE_BUILD_DIR)
+	@make $(USER_GUIDE_DIR)/build
+
+$(USER_GUIDE_DIR)/build:
+	@ln -s $(GUIDE_BUILD_DIR) $(USER_GUIDE_DIR)/build
 
 $(DOCS_PROD_DIR):
-	mkdir -p $(DOCS_PROD_DIR)
+	@mkdir -p $(DOCS_PROD_DIR)
 
 $(SLATE_GIT_HACK):
-	-@ln -s $(ROOT_DIR)/.git $(USER_GUIDE_DIR)
+	@make $(USER_GUIDE_DIR)/.git
+
+$(USER_GUIDE_DIR)/.git:
+	@ln -s $(ROOT_DIR)/.git $(USER_GUIDE_DIR)
 
 user-guide-setup:
 	@echo "\nInstalling and setting up dependencies ..."
@@ -53,7 +71,7 @@ user-guide-dev:
 
 clean-docs:
 	@echo "\nCleaning build directory ..."
-	@rm -rf $(GUIDE_BUILD_DIR)
+	@rm -rf $(DOCS_BUILD_DIR)
 
 user-guide: $(SLATE_GIT_HACK)
 	@echo "\nBuilding user guide ...\n"
@@ -61,8 +79,9 @@ user-guide: $(SLATE_GIT_HACK)
 
 setup-temp-repo: $(SLATE_GIT_HACK)
 	@echo "\nSetting up temporary git repos for gh-pages ...\n"
-	@rm -rf $(DOCS_PROD_DIR)/$(CURRENT_VERSION) $(DOCS_PROD_DIR)/.git $(DOCS_PROD_DIR)/*/.git
-	@cp -r $(GUIDE_BUILD_DIR) $(DOCS_PROD_DIR)/$(CURRENT_VERSION)
+	@rm -rf $(GUIDE_PROD_DIR) $(API_PROD_DIR) $(DOCS_PROD_DIR)/.git $(DOCS_PROD_DIR)/*/.git $(DOCS_PROD_DIR)/*/*/.git
+	@mkdir -p $(CURRENT_PROD_DIR)
+	@cp -r $(DOCS_BUILD_DIR)/* $(CURRENT_PROD_DIR)
 	@cd $(DOCS_PROD_DIR) && git init
 	@cd $(DOCS_PROD_DIR) && git add * > /dev/null
 	@cd $(DOCS_PROD_DIR) && git commit -a -m "Generated content." > /dev/null
@@ -70,5 +89,5 @@ setup-temp-repo: $(SLATE_GIT_HACK)
 teardown-temp-repo:
 	@echo "\nTearing down temporary gh-pages repos ..."
 	@rm $(USER_GUIDE_DIR)/.git $(USER_GUIDE_DIR)/Gemfile.lock
-	@rm -rf $(DOCS_PROD_DIR)/.git $(DOCS_PROD_DIR)/*/.git
+	@rm -rf $(DOCS_PROD_DIR)/.git $(DOCS_PROD_DIR)/*/.git $(DOCS_PROD_DIR)/*/*/.git
 
